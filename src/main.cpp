2c56f14e3b8ea965d01e8db7eec4077e350b1762
Vladimir Ivanov <ivanov.vladimir102002@gmail.com>
2022-03-28 12:24:41 +0300

    Parsing and transpiling moved from main to functions

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,26 @@
 
 extern std::vector<Node*> code;
 
-int main(int argc, char** argv) {
+void parse_code(std::ifstream& in_file) {
     std::string line;
-    std::string in_file_path = "examples/a.cpm";
-    std::string out_file_path = "out/a.exe";
-    std::ifstream infile(in_file_path);
+    while (std::getline(in_file, line)) {
+        yy_scan_string(line.c_str());
+        yyparse();
+    }
+}
+
+void parse_file(std::string& file_path) {
+    std::ifstream infile(file_path);
+    parse_code(infile);
+    infile.close();
+}
+
+void generate_cpp(std::ostream& outfile) {
     std::array<int, 4> errors; // 0 - redeclaration of const, 1 - redeclaration of variable, 2 - usage of undefined identifier, 3 - redifinition of const
     errors.fill(0);
     std::unordered_set<std::string> consts;
     std::unordered_set<std::string> vars_defined;
     std::unordered_set<std::string> vars_declared;
-    while (std::getline(infile, line)) {
-        yy_scan_string(line.c_str());
-        yyparse();
-    }
-    infile.close();
-    std::ofstream outfile("out/a.cpp");
     outfile << "#include \"mixed.h\"\n\n";
     outfile << "int main() {\n";
     for (Node* line : code) {
@@ -38,7 +42,19 @@ int main(int argc, char** argv) {
     outfile << "Redeclaration of variable: " << errors[1] << "\n";
     outfile << "Usage of undefined identifier: " << errors[2] << "\n";
     outfile << "Redifinition of constant: " << errors[3] << "\n*/";
+}
+
+void write_cpp(std::string& file_path) {
+    std::ofstream outfile(file_path);
+    generate_cpp(outfile);
     outfile.close();
+}
+
+int main(int argc, char** argv) {
+    std::string in_file_path = "examples/a.cpm";
+    std::string out_file_path = "out/a.cpp";
+    parse_file(in_file_path);
+    write_cpp(out_file_path);
     system("g++ out/a.cpp out/libmixed.so -o out/a.o");
     return 0;
 }
