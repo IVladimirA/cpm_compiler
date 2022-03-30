89df589fd84466ad9a6257a4b186e25c9c8444a2
Vladimir Ivanov <ivanov.vladimir102002@gmail.com>
2022-03-30 21:33:18 +0300

    Comments updated

--- src/main.cpp
+++ src/main.cpp
@@ -7,72 +7,77 @@
 #include "parser/node.tab.h"
 #include "parser/node.lexer.h"
 
+// Program's parsed commands
 extern std::vector<const Node*> code;
 
+// Print program usage
 static void print_usage(const std::string& program_name) {
     std::cout << "Usage: " << program_name << " FILE [DIRECTORY]\n"
-              << "Options:\n"
-              << "\t-h, --help\tShow this help message\n"
-              << "Specify the path to .cpm source FILE to create DIRECTORY with a.cpp and a.out files. Directory is \"./out\" by default."
-              << std::endl;
+        << "Options:\n"
+        << "\t-h, --help\tShow this help message\n"
+        << "Specify the path to .cpm source FILE to create DIRECTORY with a.cpp and a.out files. Directory is \"./out\" by default.\n";
 }
 
-static void parse_code(std::ifstream& infile) {
+// Parse code of source file
+static void parse_code(std::ifstream& in_file) {
     std::stringstream all_code;
-    all_code << infile.rdbuf();
+    all_code << in_file.rdbuf();
     yy_scan_string(all_code.str().c_str());
     yyparse();
 }
 
+// Open source file and run parser
 static void parse_file(const std::string& file_path) {
-    std::ifstream infile(file_path);
-    parse_code(infile);
-    infile.close();
+    std::ifstream in_file(file_path);
+    parse_code(in_file);
+    in_file.close();
 }
 
-static int generate_cpp(std::ostream& outfile) {
-    std::array<int, 4> errors; // stores counts of errors
+// Generate code and write it to .cpp file
+static int generate_cpp(std::ostream& out_file) {
+    std::array<int, 4> errors; // Stores counts of errors
     errors.fill(0);
-    /* 
-        0 - redeclaration of constant
-        1 - redeclaration of variable
-        2 - usage of undefined identifier
-        3 - redifinition of constant
-    */
+    // 0 - redeclaration of constant
+    // 1 - redeclaration of variable
+    // 2 - usage of undefined identifier
+    // 3 - redifinition of constant
     
-    std::unordered_set<std::string> consts; // identifiers of defined consts
-    std::unordered_set<std::string> vars_defined; // identifiers of defined variables
-    std::unordered_set<std::string> vars_declared; // identifiers of declared variables
+    
+    std::unordered_set<std::string> consts; // Identifiers of defined constants
+    std::unordered_set<std::string> vars_defined; // Identifiers of defined variables
+    std::unordered_set<std::string> vars_declared; // Identifiers of declared variables
 
-    /* Printing code to .cpp file */
-
-    outfile << "#include \"../mixed.h\"\n\n";
-    outfile << "int main() {";
-    int wrong_commands = 0; // Commands with errors count
+    // Error checking, code generating and writing
+    out_file << "#include \"../mixed.h\"\n\n";
+    out_file << "int main() {";
+    int wrong_commands = 0; // Count of commands with errors
     for (const Node* command : code) {
         if (command->check_command(consts, vars_declared, vars_defined, errors)) {
             ++wrong_commands;
         } else
-            outfile << command->generate_command();
+            out_file << command->generate_command();
         delete command;
     }
-    outfile << "\nreturn 0;\n}\n";
+    out_file << "\nreturn 0;\n}\n";
+
+    // Output of failure message
     if (wrong_commands > 0) {
-        std::cout << "Compilation failed\n";
-        std::cout << "Command(s) with errors: " << wrong_commands << "\n";
-        std::cout << "Redeclaration(s) of constant: " << errors[0] << "\n";
-        std::cout << "Redeclaration(s) of variable: " << errors[1] << "\n";
-        std::cout << "Usage(s) of undefined identifier: " << errors[2] << "\n";
-        std::cout << "Redifinition(s) of constant: " << errors[3] << "\n";
+        std::cout << "Compilation failed"
+            << "\nCommand(s) with errors: " << wrong_commands
+            << "\nRedeclaration(s) of constant: " << errors[0]
+            << "\nRedeclaration(s) of variable: " << errors[1]
+            << "\nUsage(s) of undefined identifier: " << errors[2]
+            << "\nRedifinition(s) of constant: " << errors[3] << "\n";
         return 1;
     }
     return 0;
 }
 
+// Open .cpp file and run code generating
 static int write_cpp(const std::string& file_path) {
-    std::ofstream outfile(file_path);
-    const int return_code = generate_cpp(outfile);
-    outfile.close();
+    std::ofstream out_file(file_path);
+    const int return_code = generate_cpp(out_file);
+    out_file.close();
     return return_code;
 }
 
@@ -88,24 +93,29 @@ int main(int argc, char** argv) {
         print_usage(program_name);
         return 0;
     }
-    const std::string out_path = (argc == 3) ? argv[2] : ".";  // resulting directory with .cpp and .out files location
+    const std::string out_path = (argc == 3) ? argv[2] : ".";  // Resulting directory with .cpp and .out files location
 
     const size_t path_ending = program_name.rfind("/");
     const std::string compiler_path = (path_ending == std::string::npos)
-        ? "./" : program_name.substr(0, path_ending + 1);  // path to compiler
+        ? "./" : program_name.substr(0, path_ending + 1);  // Path to C+- compiler
 
-    parse_file(source_path); // parsing source code
-    system(("mkdir -p " + compiler_path + "out").c_str()); // creating "out" directory
-    const int return_code = write_cpp(compiler_path + "out/a.cpp"); // writing transpiled code to .cpp file
+    parse_file(source_path); // Parsing source code
+    system(("mkdir -p " + compiler_path + "out").c_str()); // Creating "out" directory
+    const int return_code = write_cpp(compiler_path + "out/a.cpp"); // Writing transpiled code to .cpp file
     if (return_code != 0) {
         system(("rm -rf " + compiler_path + "out").c_str());
         return 1;
     }
-    system(("g++ " + compiler_path + "out/a.cpp " + compiler_path + "libmixed.a -o" + compiler_path + "out/a.out").c_str()); // compiling a.cpp into a.out file
+
+    // Compiling a.cpp into a.out file
+    system(("g++ " + compiler_path + "out/a.cpp " + compiler_path + "libmixed.a -o" + compiler_path + "out/a.out").c_str());
     std::cout << compiler_path << "out/a.out successfully compiled\n";
+
+    // Moving out directory to destination location
     if (system(("mv " + compiler_path + "out " + out_path).c_str()) == 0) {
         std::cout << compiler_path << "out successfully moved to " << out_path << "\n";
         return 0;
     }
     std::cout << "Couldn't moved " << compiler_path << "out to " << out_path << "\n";
+    return 1;
 }
