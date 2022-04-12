53ea84bf43cebd74ff703ab4bcaf27317cbe3b06
Vladimir Ivanov <ivanov.vladimir102002@gmail.com>
2022-04-13 00:44:08 +0300

    message of undefined identifier changed

--- src/error/error.cpp
+++ src/error/error.cpp
@@ -13,7 +13,7 @@ std::string VariableRedeclaration::get_message() const {
 }
 
 std::string UndefinedIdentifier::get_message() const {
-    return "usage of undeclared identifier \'" + information[0] + "\'";
+    return "usage of undefined identifier \'" + information[0] + "\'";
 }
 
 std::string ConstantRedefinition::get_message() const {
